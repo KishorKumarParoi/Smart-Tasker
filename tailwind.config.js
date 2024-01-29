e8c9bd853077615f405785acb7a4a45026316dd4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-30 02:33:18 +0600

    settings for typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  mode: "jit",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
